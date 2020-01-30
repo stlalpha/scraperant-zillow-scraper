c6db83de5f5b20dc0a769eecf077e716560ed9a2
mariano <marianomartinez84@gmail.com>
2020-01-30 12:18:19 -0300

    add script to run scraper

---
+++ zillow_scraper/run_scraper.py
@@ -0,0 +1,14 @@
+import argparse
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+# This scrapper takes arguments. At least zillow url is required
+p = argparse.ArgumentParser()
+p.add_argument('--zillow-url', dest='zillow_url', required=True)
+p.add_argument('--sample-mode', dest='sample_mode', action='store_true', default=False)
+
+if __name__ == '__main__':
+    args = p.parse_args()
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('zillow_spider', **vars(args))
+    process.start()
